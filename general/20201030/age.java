03698bdab9e7efc899aa5980a2b758bd32b0e810
kyoko <edgwcnn4869@gmail.com>
2020-12-09 17:23:35 +0900

    age更新

--- general/20201030/age.java
+++ general/20201030/age.java
@@ -4,11 +4,15 @@ public class age{
 		Scanner sc = new Scanner(System.in);
 		System.out.print("あなたの年齢を入力>");
 		int age = sc.nextInt();
-		ageCheck(age);
+		boolean ans =  ageCheck(age);
+		if(ans){
+			System.out.println("あなたは大人です");
+		}else{
+			System.out.println("あなたは子供です");
+		}
 	}
-	public static void ageCheck(int age){
-		String ans = (age < 20 ? "あなたは子供です":"あなたは大人です");
-		System.out.println(ans);
-
+	public static boolean ageCheck(int age){
+		boolean ans = age >= 20 ? true : false;
+		return ans;
 	}
 }
